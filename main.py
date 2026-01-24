b39a331b189fb04ba42e25b9ec867ededf0854c8
Mohanad139 <mohanadsaeed5@gmail.com>
2026-01-24 16:10:40 -0500

    Added cors

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000", 
-        "https://your-vercel-app.vercel.app",  
+        "https://getajob-frontend.vercel.app",  
     ],
     allow_credentials=True,
     allow_methods=["*"],
